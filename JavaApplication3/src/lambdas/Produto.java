2060406fa68ada6965b23d24b5e7c5fa9869ba2b
Lucas Silva <lucasmaciel6690@gmail.com>
2022-07-03 11:48:23 -0300

    Feature: Interface funcional

---
+++ JavaApplication3/src/lambdas/Produto.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lambdas;
+
+/**
+ *
+ * @author lucas
+ */
+public class Produto {
+    final String nome;
+    final double preco;
+    final double desconto;
+
+    public Produto(String nome, double preco, double desconto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+    
+    
+}
